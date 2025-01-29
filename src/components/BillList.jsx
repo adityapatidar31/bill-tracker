e2c37323698da30e4e93b95b10b11c0d9851063d
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-29 07:59:10 +0530

    create a filter and total logic

--- src/components/BillList.jsx
+++ src/components/BillList.jsx
@@ -7,13 +7,22 @@ import { BiCategoryAlt } from "react-icons/bi";
 import { MdDescription } from "react-icons/md";
 
 const BillList = () => {
-  const bills = useSelector((state) => state.bill.bills);
+  const bills = useSelector((state) => state.bill);
+
+  const { filter } = bills;
+  const filteredBills = bills.bills.filter(
+    (bill) => bill.category === "" || filter == ""
+  );
+
+  const totalAmount = filteredBills.reduce(
+    (total, bill) => total + bill.amount,
+    0
+  );
   const iconStyle = {
     marginRight: "8px",
     fontSize: "16px",
     color: "var(--text-color)",
   };
-
   return (
     <div className="list-container">
       <h2>Expense List</h2>
@@ -34,9 +43,10 @@ const BillList = () => {
         </div>
         <div className="item-actions">Action</div>
       </div>
-      {bills.map((bill) => (
+      {filteredBills.map((bill) => (
         <BillListItem key={bill.id} {...bill} />
       ))}
+      <h2>Total Amount: {totalAmount}</h2>
     </div>
   );
 };
