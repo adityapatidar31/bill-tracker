c8a9ad8f4ab4b673b888146ace9b313f50e464f4
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-29 05:59:43 +0530

    install react icon and adjust the css of the list

--- src/components/BillList.jsx
+++ src/components/BillList.jsx
@@ -5,11 +5,18 @@ const ExpenseList = () => {
   const bills = useSelector((state) => state.bill.bills);
 
   return (
-    <>
+    <div>
+      <div className="item-row">
+        <div className="item-amount">Amount</div>
+        <div className="item-description">Description</div>
+        <div className="item-category">Category</div>
+        <div className="item-date">Date</div>
+        <div className="item-actions"></div>
+      </div>
       {bills.map((bill) => (
         <BillListItem key={bill.id} {...bill} />
       ))}
-    </>
+    </div>
   );
 };
 
