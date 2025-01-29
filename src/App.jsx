ed654fb1f5ab41488c69b3035a12d26c978038e7
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-01-29 06:13:42 +0530

    add dark theam

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import BillList from "./components/BillList";
 import Form from "./components/Form";
 import Graph from "./components/Graph";
 import PieChart from "./components/PieChart";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  // Toggle the theme
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
     <div className="app">
+      <header>
+        <button onClick={toggleTheme}>
+          Switch to {theme === "light" ? "Dark" : "Light"} Theme
+        </button>
+      </header>
       <Form />
       <BillList />
       <Graph />
--- src/components/BillList.jsx
+++ src/components/BillList.jsx
@@ -1,17 +1,36 @@
 import { useSelector } from "react-redux";
 import BillListItem from "./BillListItem";
+import { FaDollarSign } from "react-icons/fa";
+
+import { AiOutlineCalendar } from "react-icons/ai";
+import { BiCategoryAlt } from "react-icons/bi";
+import { MdDescription } from "react-icons/md";
 
 const ExpenseList = () => {
   const bills = useSelector((state) => state.bill.bills);
-
+  const iconStyle = {
+    marginRight: "8px",
+    fontSize: "16px",
+    color: "var(--text-color)",
+  };
   return (
     <div>
       <div className="item-row">
-        <div className="item-amount">Amount</div>
-        <div className="item-description">Description</div>
-        <div className="item-category">Category</div>
-        <div className="item-date">Date</div>
-        <div className="item-actions"></div>
+        <div className="item-amount">
+          <FaDollarSign style={iconStyle} /> Amount
+        </div>
+        <div className="item-description">
+          <MdDescription style={iconStyle} />
+          Description
+        </div>
+        <div className="item-category">
+          <BiCategoryAlt style={iconStyle} /> Category
+        </div>
+        <div className="item-date">
+          <AiOutlineCalendar style={iconStyle} />
+          Date
+        </div>
+        <div className="item-actions">Action</div>
       </div>
       {bills.map((bill) => (
         <BillListItem key={bill.id} {...bill} />
